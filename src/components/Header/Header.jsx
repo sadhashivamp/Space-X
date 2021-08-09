f8e6c2d214b7c7f641e7ce8ae85fafee705f97d2
sadhashivamp <ramulu320p@gmail.com>
2021-08-09 08:45:50 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+class Header extends React.Component {
+    render() {
+        return(
+            <div className = "Header">
+                <h1>welcome to space-x Application</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export class Header extends React.Component {
+    render() {
+        return(
+            <div >
+                welcome to space-x Application
+            </div>
+        );
+    }
+}
+
